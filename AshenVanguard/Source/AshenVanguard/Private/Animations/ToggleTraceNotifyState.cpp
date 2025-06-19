f20ec10fab7f36af2c2a393c8489196b13a7e5f8
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-06-20 01:55:32 +0530

    Continuous Tracing Stopped

--- AshenVanguard/Source/AshenVanguard/Private/Animations/ToggleTraceNotifyState.cpp
+++ AshenVanguard/Source/AshenVanguard/Private/Animations/ToggleTraceNotifyState.cpp
@@ -1,10 +1,19 @@
 #include "Animations/ToggleTraceNotifyState.h"
 #include "Combat/TraceComponent.h"
 
+UTraceComponent* UToggleTraceNotifyState::GetTraceComponent(USkeletalMeshComponent* MeshComp) const
+{
+	if (!IsValid(MeshComp) || !IsValid(MeshComp->GetOwner()))
+	{
+		return nullptr;
+	}
+
+	return MeshComp->GetOwner()->FindComponentByClass<UTraceComponent>();
+}
 
 void UToggleTraceNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
 {
-	UTraceComponent* TraceComp{MeshComp->GetOwner()->FindComponentByClass<UTraceComponent>() };
+	UTraceComponent* TraceComp = GetTraceComponent(MeshComp);
 
 	if (!IsValid(TraceComp)) { return; }
 
@@ -13,7 +22,7 @@ void UToggleTraceNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAni
 
 void UToggleTraceNotifyState::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
 {
-	UTraceComponent* TraceComp{MeshComp->GetOwner()->FindComponentByClass<UTraceComponent>() };
+	UTraceComponent* TraceComp = GetTraceComponent(MeshComp);
 
 	if (!IsValid(TraceComp)) { return; }
 
--- AshenVanguard/Source/AshenVanguard/Public/Animations/ToggleTraceNotifyState.h
+++ AshenVanguard/Source/AshenVanguard/Public/Animations/ToggleTraceNotifyState.h
@@ -4,11 +4,13 @@
 #include "Animation/AnimNotifies/AnimNotifyState.h"
 #include "ToggleTraceNotifyState.generated.h"
 
+class UTraceComponent;
+
 UCLASS()
 class ASHENVANGUARD_API UToggleTraceNotifyState : public UAnimNotifyState
 {
 	GENERATED_BODY()
-	
+
 public:
 	virtual void NotifyBegin(
 		USkeletalMeshComponent* MeshComp,
@@ -22,4 +24,7 @@ public:
 		UAnimSequenceBase* Animation,
 		const FAnimNotifyEventReference& EventReference
 	) override;
+
+private:
+	UTraceComponent* GetTraceComponent(USkeletalMeshComponent* MeshComp) const;
 };
