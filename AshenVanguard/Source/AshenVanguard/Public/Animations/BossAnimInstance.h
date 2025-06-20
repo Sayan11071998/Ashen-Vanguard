a0f0326b8136f394952c4eae76f592e3c1c7510d
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-06-21 04:36:15 +0530

    Working on Boss

---
+++ AshenVanguard/Source/AshenVanguard/Public/Animations/BossAnimInstance.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BossAnimInstance.generated.h"
+
+UCLASS()
+class ASHENVANGUARD_API UBossAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float currentSpeed{ 0.0f };
+};
