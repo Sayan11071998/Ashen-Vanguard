5de6460e2e43d235ff849735c5841c6b8c56ff92
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-06-26 01:45:51 +0530

    Player Distance BT Service Added

--- AshenVanguard/Intermediate/Build/Win64/UnrealEditor/Inc/AshenVanguard/UHT/AshenVanguard.init.gen.cpp
+++ AshenVanguard/Intermediate/Build/Win64/UnrealEditor/Inc/AshenVanguard/UHT/AshenVanguard.init.gen.cpp
@@ -25,13 +25,13 @@ void EmptyLinkFunctionForGeneratedCodeAshenVanguard_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x45A23D96,
-				0x8FF8DEA2,
+				0x558FEF1E,
+				0xF6031952,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_AshenVanguard.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_AshenVanguard.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_AshenVanguard(Z_Construct_UPackage__Script_AshenVanguard, TEXT("/Script/AshenVanguard"), Z_Registration_Info_UPackage__Script_AshenVanguard, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x45A23D96, 0x8FF8DEA2));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_AshenVanguard(Z_Construct_UPackage__Script_AshenVanguard, TEXT("/Script/AshenVanguard"), Z_Registration_Info_UPackage__Script_AshenVanguard, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x558FEF1E, 0xF6031952));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ AshenVanguard/Intermediate/Build/Win64/UnrealEditor/Inc/AshenVanguard/UHT/BTS_PlayerDistance.generated.h
@@ -0,0 +1,56 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Characters/AI/BTS_PlayerDistance.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ASHENVANGUARD_BTS_PlayerDistance_generated_h
+#error "BTS_PlayerDistance.generated.h already included, missing '#pragma once' in BTS_PlayerDistance.h"
+#endif
+#define ASHENVANGUARD_BTS_PlayerDistance_generated_h
+
+#define FID_Users_sayan_Projects_Ashen_Vanguard_AshenVanguard_Source_AshenVanguard_Public_Characters_AI_BTS_PlayerDistance_h_10_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUBTS_PlayerDistance(); \
+	friend struct Z_Construct_UClass_UBTS_PlayerDistance_Statics; \
+public: \
+	DECLARE_CLASS(UBTS_PlayerDistance, UBTService, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/AshenVanguard"), NO_API) \
+	DECLARE_SERIALIZER(UBTS_PlayerDistance)
+
+
+#define FID_Users_sayan_Projects_Ashen_Vanguard_AshenVanguard_Source_AshenVanguard_Public_Characters_AI_BTS_PlayerDistance_h_10_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UBTS_PlayerDistance(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UBTS_PlayerDistance(UBTS_PlayerDistance&&); \
+	UBTS_PlayerDistance(const UBTS_PlayerDistance&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UBTS_PlayerDistance); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UBTS_PlayerDistance); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UBTS_PlayerDistance) \
+	NO_API virtual ~UBTS_PlayerDistance();
+
+
+#define FID_Users_sayan_Projects_Ashen_Vanguard_AshenVanguard_Source_AshenVanguard_Public_Characters_AI_BTS_PlayerDistance_h_7_PROLOG
+#define FID_Users_sayan_Projects_Ashen_Vanguard_AshenVanguard_Source_AshenVanguard_Public_Characters_AI_BTS_PlayerDistance_h_10_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_sayan_Projects_Ashen_Vanguard_AshenVanguard_Source_AshenVanguard_Public_Characters_AI_BTS_PlayerDistance_h_10_INCLASS_NO_PURE_DECLS \
+	FID_Users_sayan_Projects_Ashen_Vanguard_AshenVanguard_Source_AshenVanguard_Public_Characters_AI_BTS_PlayerDistance_h_10_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ASHENVANGUARD_API UClass* StaticClass<class UBTS_PlayerDistance>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_sayan_Projects_Ashen_Vanguard_AshenVanguard_Source_AshenVanguard_Public_Characters_AI_BTS_PlayerDistance_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
